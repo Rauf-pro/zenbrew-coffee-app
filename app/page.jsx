9b92ea7d133a7f2a07e9fc3da37720802c1e5ca9
rrauf8178 <rrauf8178@gmail.com>
2024-08-21 21:38:17 +0400

    add: separator component

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,13 @@
 
 import Hero from "@/components/Hero";
 
-
 const Home = () => {
+  // add locomotive scroll
   return (
     <div className="h-full overflow-x-hidden">
       <Hero />
+      {/* temporary div */}
+      <div className="h-[4000px]"></div>
     </div>
   );
 };
--- components/Separator.jsx
+++ components/Separator.jsx
@@ -1,5 +1,15 @@
-const Separator = () => {
-  return <div>Separator</div>;
+import Image from "next/image";
+
+const Separator = ({ bg = "white" }) => {
+  const imgSrc =
+    bg === "accent"
+      ? "/assets/separator-accent.svg"
+      : "/assets/separator-white.svg";
+  return (
+    <div className="relative w-[168px] h-[26px] mx-auto">
+      <Image src={imgSrc} alt="" fill />
+    </div>
+  );
 };
 
 export default Separator;
