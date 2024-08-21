64ec24d355a9fc90ebb13b81a8fa36b186b2eee3
rrauf8178 <rrauf8178@gmail.com>
2024-08-21 19:28:03 +0400

    add: font-family, colors, bg images, screens to tailwind config for using as variable

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Hero from "@/components/Hero";
+
+
+const Home = () => {
+  return (
+    <div className="h-full overflow-x-hidden">
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
