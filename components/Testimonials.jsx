49916f202809323c114068d1052a136aef105bf8
rrauf8178 <rrauf8178@gmail.com>
2024-08-23 13:49:55 +0400

    add: testiomonilas component fully style

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -34,7 +34,32 @@ const testimonials = [
 ];
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <section className="h-[60vh] xl:h-[70vh]">
+      <div className="container mx-auto h-full flex items-center">
+        <Swiper navigation={true} modules={[Navigation]} className="">
+          {testimonials.map((person, index) => {
+            return (
+              <SwiperSlide key={index} className="w-full h-full">
+                <div className="flex justify-center h-full xl:pt-14">
+                  <div className="max-w-[60%] text-primary">
+                    <IoMdQuote className="text-6xl text-primary mb-12 mx-auto" />
+                    <p className="text-2xl font-secondary text-center mb-8">
+                      {person.message}
+                    </p>
+                    <div className="text-center">
+                      <p className="text-xl font-bold mb-1">{person.name}</p>
+                      <p className="text-secondary">{person.profession}</p>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
